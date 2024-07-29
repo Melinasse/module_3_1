69c67af1b502b6d3c8d2de77696f99fe38eb8315
Azam4t17 <AyzekMendes@gmail.com>
2024-07-29 20:53:24 +0500

    Update module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -1,47 +1,41 @@
 calls = 0
+
+
 def count_calls():
     global calls
     calls += 1
+
+
 print(f'Количество вызова функции: {calls} раз(а)')
+
+
 def string_info(string):
     global calls
-    calls += 1
-    # str = input('Введите тескст: ') # Для ручного ввода текста
+    count_calls()
     length = len(string)
     upper_string = string.upper()
     lower_string = string.lower()
     return (length, lower_string, upper_string)
     string_info('Capybara')
     string_info('Armageddon')
+
+
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
+
 print(f'Количество вызова функции: {calls} раз(а)')
-# result = string_info(str) # Вывод ручного ввода
-# print(result) # Вывод ручного ввода
+
+
 def is_contains(string, list_to_search):
     global calls
-    calls += 1
+    count_calls()
     lower_string = string.lower()
     list_to_search = [item.lower() for item in list_to_search]
     return lower_string in list_to_search
     is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])
     is_contains('cycle', ['recycling', 'cyclic'])
+
+
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
 print(is_contains('cycle', ['recycling', 'cyclic']))
 print(f'Количество вызова функции: {calls} раз(а)')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
